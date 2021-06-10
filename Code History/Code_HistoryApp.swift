54c0357974dfd29d4130b50ded0d1fd3a0b4b909
Oliver van den Bosch <74139069+ovandenbosch@users.noreply.github.com>
2021-06-10 15:27:13 +0100

    Initial Commit

---
+++ Code History/Code_HistoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  Code_HistoryApp.swift
+//  Code History
+//
+//  Created by Oliver van den Bosch on 6/10/21.
+//
+
+import SwiftUI
+
+@main
+struct Code_HistoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
