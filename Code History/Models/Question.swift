1cc07dd3d93e1adb0d87d79bacd830999c1c351d
Oliver van den Bosch <74139069+ovandenbosch@users.noreply.github.com>
2021-06-20 21:33:30 +0100

    Create hash table

--- Code History/Models/Question.swift
+++ Code History/Models/Question.swift
@@ -7,9 +7,28 @@
 
 import Foundation
 
-struct Question {
+struct Question: Hashable {
     let questionText: String
     let possibleAnswers: [String]
     let correctAnswerIndex: Int
     
+    static var allQuestions = [
+        Question(questionText: "Who invented the World Wide Web?",
+                      possibleAnswers: [
+                        "Steve Jobs",
+                        "Linus Torvalds",
+                        "Bill Gates",
+                        "Tim Berners-Lee"
+                      ],
+                      correctAnswerIndex: 3),
+        Question(questionText: "What was the first object orientated programming language?",
+                      possibleAnswers: [
+                        "Simula",
+                        "C",
+                        "Assembly",
+                        "Fortan"
+                      ],
+                      correctAnswerIndex: 0),
+        
+    ]
 }
