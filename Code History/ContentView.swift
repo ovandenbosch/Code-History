128e2cc79c4d729679bb93b10b0ec4ff7c6b3e32
Oliver van den Bosch <74139069+ovandenbosch@users.noreply.github.com>
2021-06-20 12:48:05 +0100

    Refactor Stack to use Foreach map and State colors.

--- Code History/ContentView.swift
+++ Code History/ContentView.swift
@@ -9,12 +9,12 @@ import SwiftUI
 
 struct ContentView: View {
     
-let question = Question(
+    let question = Question(
     questionText: "What was the first computer bug?",
     possibleAnswers: ["Ant", "Beetle", "Moth", "Fly"],
     correctAnswerIndex: 2)
     
-    let mainColor = Color(red: 20/255, green: 28/255, blue: 58/255)
+    @State var mainColor = Color(red: 20/255, green: 28/255, blue: 58/255)
     
     var body: some View {
         ZStack {
@@ -30,26 +30,14 @@ let question = Question(
                     .multilineTextAlignment(/*@START_MENU_TOKEN@*/.leading/*@END_MENU_TOKEN@*/)
                 Spacer()
                 HStack {
-                    Button(action: {
-                        print("Tapped on choice 1")
-                    }, label: {
-                        ChoiceTextView(choiceText: question.possibleAnswers[0])
-                    })
-                    Button(action: {
-                        print("Tapped on choice 2")
-                    }, label: {
-                        ChoiceTextView(choiceText: question.possibleAnswers[1])
-                    })
-                    Button(action: {
-                        print("Tapped on choice 3")
-                    }, label: {
-                        ChoiceTextView(choiceText: question.possibleAnswers[2])
-                    })
-                    Button(action: {
-                        print("Tapped on choice 4")
-                    }, label: {
-                        ChoiceTextView(choiceText: question.possibleAnswers[3])
-                    })
+                    ForEach(0..<question.possibleAnswers.count) { answerIndex in
+                        Button(action: {
+                            print("Tapped on option with the text: \(question.possibleAnswers[answerIndex])")
+                            mainColor = answerIndex == question.correctAnswerIndex ? .green : .red
+                        }, label: {
+                            ChoiceTextView(choiceText: question.possibleAnswers[answerIndex])
+                        })
+                    }
                 }
             }
         }
